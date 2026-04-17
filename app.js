5a51747b049e49bde88a71348a85e964c9236b21
Sara Ayumi <seuemail@gmail.com>
2026-04-17 00:33:55 -0300

    update

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const porta = process.env.PORT || 10000;
 
 app.use(cors());
 
+// 🖼️ (opcional) servir imagens locais
+app.use("/images", express.static("images"));
+
 const bleach = [
   {
     id: 1,
@@ -13,7 +16,8 @@ const bleach = [
     raca: "Alma Modificada",
     cargo: "Nulo",
     zanpakuto: "Nulo",
-    status: "Vivo"
+    status: "Vivo",
+    imagem: "https://static.wikia.nocookie.net/all-worlds-alliance/images/f/f9/75cdd01890cec27b3b1e862a73a64a50.png/revision/latest/scale-to-width-down/479?cb=20200504070203"
   },
   {
     id: 2,
@@ -21,7 +25,8 @@ const bleach = [
     raca: ["Shinigami", "Hollow", "Quincy", "Humano", "Arrancar"],
     cargo: "Substituto",
     zanpakuto: "Zangetsu",
-    status: "Vivo"
+    status: "Vivo",
+    imagem: "https://static.wikitide.net/deathbattlewiki/f/f6/Portrait.ichigokurosaki.png"
   },
   {
     id: 3,
@@ -29,7 +34,8 @@ const bleach = [
     raca: "Shinigami",
     cargo: "Capitã da 13° Divisão",
     zanpakuto: "Sode no Shirayuki",
-    status: "Vivo"
+    status: "Vivo",
+    imagem: "https://upload.wikimedia.org/wikipedia/pt/thumb/9/9c/Rukia.jpg/250px-Rukia.jpg"
   },
   {
     id: 4,
@@ -37,7 +43,8 @@ const bleach = [
     raca: "Humano",
     cargo: "Nulo",
     zanpakuto: "Nulo",
-    status: "Vivo"
+    status: "Vivo",
+    imagem: "https://www.writeups.org/wp-content/uploads/Yasutora-Chad-Sado-Bleach-Shonen-Jump.jpg"
   },
   {
     id: 5,
@@ -45,7 +52,8 @@ const bleach = [
     raca: "Shinigami",
     cargo: "Tenente da 6° Divisão",
     zanpakuto: "Zabimaru",
-    status: "Vivo"
+    status: "Vivo",
+    imagem: "https://static.wikia.nocookie.net/p__/images/b/b8/Bleach_TYBW_Renji_Anime_Render.png/revision/latest?cb=20221212185324&path-prefix=protagonist"
   },
   {
     id: 6,
@@ -53,7 +61,8 @@ const bleach = [
     raca: "Arrancar",
     cargo: "Espada N° 6",
     zanpakuto: "Pantera",
-    status: "Vivo"
+    status: "Vivo",
+    imagem: "https://i.pinimg.com/736x/cb/d5/a2/cbd5a226e96700617488787652777299.jpg"
   }
 ];
 
@@ -79,7 +88,7 @@ app.get("/bleach/:id", (req, res) => {
   res.json(personagem);
 });
 
-// 🔎 por nome (BUSCA FLEXÍVEL)
+// 🔎 por nome
 app.get("/bleach/nome/:nome", (req, res) => {
   const nome = req.params.nome.toLowerCase();
 
@@ -112,7 +121,7 @@ app.get("/bleach/raca/:raca", (req, res) => {
   res.json(resultado);
 });
 
-// 🚀 servidor (SÓ UM!)
+// 🚀 servidor
 app.listen(porta, () => {
   console.log(`Servidor rodando na porta ${porta}`);
 });
